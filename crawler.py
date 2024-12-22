b45e7d89680f8465a3319325dae4bc75cae8abd5
rainbow_yuyu <465936509@qq.com>
2024-12-22 22:09:14 +0800

    rainbow_yu

--- crawler.py
+++ crawler.py
@@ -4,16 +4,73 @@ from bs4 import BeautifulSoup
 import time
 import re
 
-# 设置Chrome驱动路径
-driver_path = 'path_to_your_chromedriver'
+ms: dict = {}
+page_num = 0
+path = 'resources/Score.txt'
+file = open(path, 'r', encoding='UTF-8')
+headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0',
+    }
+json_data = {
+    "page": page_num,
+    "limit": 20,
+    "degree_type": "",
+    "level1": "",
+    "level2": "",
+    "special_name": ""
+}
 
-# 初始化Selenium WebDriver
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')  # 无头模式
-driver = webdriver.Chrome(executable_path=driver_path, options=options)
+class Score:
+    data: dict = {
+        'scoreYear': int,
+        'scoreAll': int,
+        'scoreEnglish': int,
+        'scorePolitics': int,
+        'scoreMath': int,
+        'scoreMajor': int,
+        'majorId': int,
+        'areaType': int
+    }
+
+    def __init__(self, s: [str]):
+        self.data['scoreYear'] = s[0]
+        majorId = ms.get(s[1])
+        if majorId is not None:
+            self.data['majorId'] = majorId
+        self.data['scoreAll'] = s[2]
+        self.data['scoreEnglish'] = s[3]
+        self.data['scorePolitics'] = s[4]
+        self.data['scoreMath'] = s[5]
+        self.data['scoreMajor'] = s[6]
+        if s[7] == 'A区\n':
+            self.data['areaType'] = 1
+        elif s[7] == 'B区\n':
+            self.data['areaType'] = 2
+
+
+def initData() -> Score or None:
+    line = file.readline()
+    s: [str] = line.split("，")
+    if len(s) == 1:
+        return None
+    return Score(s)
+
+
+def request(m: Score):
+    response = requests.post(url, headers=headers, json=m.data)
+    if response.ok:
+        pass
+    else:
+        exit(1)
+
+driver = webdriver.Edge()
+driver.get("https://www.bb.com/news")
+driver.maximize_window()
+driver.execute_script("document.documentElement.scrollTop=1800")
+driver.find_element("XPATH", '//*[@id="btn"]/span').click()
 
 # 访问考研国家线网页
-url = 'https://www.example.com/kaoyan-guojiaxian'  # 替换为实际的页面URL
+url = 'https://y.si.com.cn/zyk'
 driver.get(url)
 
 # 等待页面加载
@@ -23,25 +80,22 @@ time.sleep(5)
 html_content = driver.page_source
 
 # 使用BeautifulSoup解析HTML
-soup = BeautifulSoup(html_content, 'html.parser')
+response = requests.get(url)
+re_li = '<li title="(.*?)">.*?</li>'
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text,'html.parser')
+    news_links = soup.find_all('ul',class_='clearfix')
+    data = re.findall(re_li,str(news_links),re.S)
 
-# 假设国家线数据存放在一个特定的表格中，找到该表格
-table = soup.find('table', {'class': 'national-line-table'})  # 替换为实际的table class名
-
-# 获取所有行数据
+table = soup.find_all('ul',class_='clearfix')
 rows = table.find_all('tr')
-
-# 使用正则表达式从每一行中提取年份和对应的国家线
 national_lines = []
 
 for row in rows:
     cols = row.find_all('td')
     if len(cols) > 1:
-        # 获取年份和国家线数据
         year = cols[0].text.strip()
         line_data = cols[1].text.strip()
-
-        # 使用正则表达式提取国家线的数字
         match = re.search(r'(\d+)', line_data)
         if match:
             national_line = match.group(1)
